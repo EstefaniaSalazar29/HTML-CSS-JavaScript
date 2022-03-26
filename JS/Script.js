0b1e2548df096c7b769ed396a57d16a50562e495
Estefania Salazar Valencia <estefaniasalazarv@iesanagustin.edu.co>
2022-03-25 19:25:17 -0500

    Workshop

---
+++ JS/Script.js
@@ -0,0 +1,117 @@
+import { characters } from "../data/data.js";
+
+
+window.addEventListener('DOMContentLoaded', () => {
+
+    //Se duplica la Data 
+
+    let charactersB = Object.values(characters);
+    let charactersC = characters.concat(charactersB);
+
+    //Se baraja las cards, para orden aleatorio
+
+    charactersC.sort(() => 0.5 - Math.random());
+
+    let puntos = document.getElementById('puntaje');
+    let rest = document.getElementById('vidas');
+
+    puntos.textContent = " $ 0 ";
+    rest.textContent = "3";
+
+    //Se crea el tablero
+    const grid = document.querySelector('.grid');
+    let cardElegida = [];
+    let cardElegidaId = [];
+    const cardsGanadas = [];
+    const vidas = [1, 2, 3];
+
+    function crearTablero() {
+
+        for (let i = 0; i < charactersC.length; i++) {
+            var card = document.createElement('img');
+            card.setAttribute('src', 'https://res.cloudinary.com/jorge-tarifa/image/upload/v1648254018/Image_from_iOS_tooqq5.jpg');
+            card.setAttribute('data-id', i);
+            card.addEventListener('click', voltearCarta)
+            grid.appendChild(card);
+        }
+
+    }
+
+    // funcion que compara las cartas
+
+    function par() {
+        let cards = document.querySelectorAll('img');
+        const optionOneId = cardElegidaId[0];
+        const optionTwoId = cardElegidaId[1];
+
+        if (optionOneId == optionTwoId) {
+
+            //Se activa cuando le das a la misma carta
+
+            cards[optionOneId].setAttribute('src', '../img/image1.png')
+            cards[optionTwoId].setAttribute('src', '../img/image1.png')
+            alert('Hey! estas dandole click a la misma carta')
+        }
+
+        else if (cardElegida[0] === cardElegida[1]) {
+
+            //Determina si las cards son iguales
+
+            alert('Encontraste un par, ¡genial!')
+            cards[optionOneId].setAttribute('src', '../img/image1.png')
+            cards[optionTwoId].setAttribute('src', '../img/image1.png')
+            cards[optionOneId].removeEventListener('click', voltearCarta)
+            cards[optionTwoId].removeEventListener('click', voltearCarta)
+            cardsGanadas.push(cardElegida, "1");
+
+
+
+        }
+
+        else {
+
+            //Determina si las cards son diferentes
+
+            alert('intenta de nuevo');
+            cards[optionOneId].setAttribute('src', '../img/image1.png')
+            cards[optionTwoId].setAttribute('src', '../img/image1.png')
+            vidas.pop()
+
+
+
+
+        }
+
+        cardElegida = []
+        cardElegidaId = []
+        rest.textContent = vidas.length;
+        puntos.textContent = cardsGanadas.length + " $";
+
+        if (vidas.length === 0) {
+
+            window.location.reload();
+            alert("has perdido todas tus vidas");
+        }
+
+    }
+
+
+    //funcion que voltea la carta
+
+    function voltearCarta() {
+
+        let cardId = this.getAttribute('data-id');
+        cardElegida.push(charactersC[cardId].name)
+        cardElegidaId.push(cardId)
+
+
+        this.setAttribute('src', charactersC[cardId].image)
+
+        if (cardElegidaId.length == 2) {
+            setTimeout(par, 500)
+        }
+
+    }
+    crearTablero()
+})
+
